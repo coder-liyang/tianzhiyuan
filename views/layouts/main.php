82885756525da383f6de75b83b1745fa3d53ad7a
Li Yang <liyangcoder@163.com>
2016-06-26 01:14:26 +0800

    db config

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -56,6 +56,13 @@ AppAsset::register($this);
         <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
+        <script type="text/javascript" src="http://tajs.qq.com/stats?sId=27997262" charset="UTF-8"></script>
+        <script src="http://s23.cnzz.com/stat.php?id=5472182&web_id=5472182&show=pic" language="JavaScript"></script>
+
+        <script type="text/javascript">
+            var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+            document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fe81c7b6d3cb8a15f352d0bfcd85b34bc' type='text/javascript'%3E%3C/script%3E"));
+        </script>
     </div>
 </footer>
 
